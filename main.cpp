c8ae2ed4c8e434d2d70611d4b6a6adb99c93f62e
Miguel Teixeira <miguel.teixeira@poli.ufrj.br>
2020-07-17 18:17:55 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
 #include <listenercontroller.h>
 #include <librasoffice_app.h>
-#include <QDebug>
-#include <QThread>
 
 
 int main(int argc, char *argv[])
 {
-     //QThread *librasoffice_signs = new QThread;
-     //PopupWorker *libras_worker = new PopupWorker(argc, argv);
-     //libras_worker->moveToThread(librasoffice_signs);
-     //QObject::connect(librasoffice_signs, SIGNAL(started()), libras_worker, SLOT(run()));
      LIBRASOffice_App *librasoffice_app = new LIBRASOffice_App(argc, argv);
-     //librasoffice_signs->start();
-    librasoffice_app->run();
+     librasoffice_app->run();
 }
